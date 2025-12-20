d2d576963fed79440bcfc55452c8d274b536d992
Simerson Sime <simersonsime1@gmail.com>
2025-12-20 19:32:24 +0300

    Allow Render host in Vite preview

--- amazon-clone-2025/vite.config.js
+++ amazon-clone-2025/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  preview: {
+    host: true, // allow external host
+    port: process.env.PORT || 4173,
+    allowedHosts: ["amazon-clone-2025-hnhv.onrender.com"],
+  },
+});
