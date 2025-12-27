13001380de83d00dfc730758160e813cb4c7e4c2
simersonsime <simersonsime1@gmail.com>
2025-12-27 19:30:23 +0300

    Update vite.config.js

--- amazon-clone-2025/vite.config.js
+++ amazon-clone-2025/vite.config.js
@@ -6,6 +6,6 @@ export default defineConfig({
   preview: {
     host: true, // allow external host
     port: process.env.PORT || 4173,
-    allowedHosts: ["amazon-clone-2025-hnhv.onrender.com"],
+    allowedHosts: ["amazon-clone-2025-hnhv.onrender.com","amazon-clone-2025-1.onrender.com"],
   },
 });
